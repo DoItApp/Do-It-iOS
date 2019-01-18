1e664ee364b88120995dac61fe6103327fce200a
Aaron Rice <awrice@calpoly.edu>
2019-01-17 17:48:06 -0800

    Added initial elements of doit cell

---
+++ Do-It/View/DoItTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DoItTableViewCell.swift
+//  Do-It
+//
+//  Created by Aaron on 1/17/19.
+//  Copyright © 2019 The Swifter Picker-Uppers. All rights reserved.
+//
+
+import UIKit
+
+class DoItTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
