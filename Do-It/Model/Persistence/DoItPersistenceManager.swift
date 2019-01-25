48a8a30d7793666aaeca4a5ab0d0498dfc2662f7
mathmch <mathmch@gmail.com>
2019-01-24 17:17:16 -0800

    pushing for pull request requirement

--- Do-It/Model/Persistence/DoItPersistenceManager.swift
+++ Do-It/Model/Persistence/DoItPersistenceManager.swift
@@ -10,18 +10,25 @@ import Foundation
 
 
 final class DoItPersistenceManager {
+    var encoder = JSONEncoder()
+    var decoder = JSONDecoder()
+    var fileManager = FileManager.default
+    var docsURL: URL
+    var doItsURL: URL
     var doIts: [DoIt] {
         didSet {
             saveDoItsToDisk()
         }
     }
-
+    // add creating file if it does not already exist
     init() {
+        docsURL = try! fileManager.url(for:.documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        doItsURL = docsURL.appendingPathComponent("DoIts.json")
         doIts = DoItPersistenceManager.loadDoItsFromDisk()
     }
 
     func save(_ doIt: DoIt) {
-
+        
     }
 
     func update(_ doIt: DoIt) {
@@ -37,6 +44,7 @@ final class DoItPersistenceManager {
     }
 
     private func saveDoItsToDisk() {
+        //let data = encoder.encode(doIts)
 
     }
 }
