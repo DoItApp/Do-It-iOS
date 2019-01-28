ac95d6f52d41fbda6c8c2f265a75005424a98e2c
mathmch <mathmch@gmail.com>
2019-01-28 10:56:25 -0800

    completed load and save function

--- Do-It/Model/Persistence/DoItPersistenceManager.swift
+++ Do-It/Model/Persistence/DoItPersistenceManager.swift
@@ -20,13 +20,15 @@ final class DoItPersistenceManager {
             saveDoItsToDisk()
         }
     }
+    
     // add creating file if it does not already exist
     init() {
-        docsURL = try! fileManager.url(for:.documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        docsURL = try! fileManager.url(for:.documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
         doItsURL = docsURL.appendingPathComponent("DoIts.json")
         doIts = DoItPersistenceManager.loadDoItsFromDisk()
     }
-
+    
+    // ask Michael what these functions should do exactly
     func save(_ doIt: DoIt) {
         
     }
@@ -40,11 +42,25 @@ final class DoItPersistenceManager {
     }
 
     private static func loadDoItsFromDisk() -> [DoIt] {
-        fatalError("not implemented")
+        // Read data from .json file and transform data into an array
+        do {
+            let docsURL = try! FileManager.default.url(for:.documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let doItsURL = docsURL.appendingPathComponent("DoIts.json")
+            let data = try Data(contentsOf: doItsURL, options: [])
+            let doItsArray =  try JSONSerialization.jsonObject(with: data, options: []) as! [DoIt]
+            return doItsArray
+        } catch {
+            print("Failed to read JSON data")
+            return []
+        }
     }
-
+    
     private func saveDoItsToDisk() {
-        //let jsonData = try encoder.encode(doIts)
-
+        do {
+            let data = try JSONSerialization.data(withJSONObject: doIts, options: [])
+            try data.write(to: doItsURL, options: [])
+        } catch {
+            print("Failed to write JSON data")
+        }
     }
 }
