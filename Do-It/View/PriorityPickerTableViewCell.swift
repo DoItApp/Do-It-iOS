716cdebb214e15b524ba62f0d3e691a4ddbd920b
Jett Moy <jettmoy@gmail.com>
2019-02-20 20:00:03 -0800

    priority segmented control

---
+++ Do-It/View/PriorityPickerTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  PriorityPickerTableViewCell.swift
+//  Do-It
+//
+//  Created by Jett Moy on 2/20/19.
+//  Copyright © 2019 The Swifter Picker-Uppers. All rights reserved.
+//
+
+import UIKit
+
+class PriorityPickerTableViewCell: UITableViewCell {
+
+    var doItPriority: DoItPriority = .default
+
+    @IBAction func selectPriority(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex
+        {
+        case 0:
+            doItPriority = .low
+        case 1:
+            doItPriority = .default
+        case 2:
+            doItPriority = .high
+        default:
+            fatalError()
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
