78d9bb4fb4cee0c74a9b3e401d2f520dd329e953
Aaron Rice <awrice@calpoly.edu>
2019-03-03 15:56:50 -0800

    can select cell for time range

--- Do-It/Controller/DueDatePickerTableViewController.swift
+++ Do-It/Controller/DueDatePickerTableViewController.swift
@@ -10,37 +10,53 @@ import UIKit
 
 class DueDatePickerTableViewController: UITableViewController {
 
+    var options = [0: "Today",
+                   1: "Tomorrow",
+                   2: "Within the next Three Days",
+                   3: "Within the Week"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return options.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DueDateCell", for: indexPath)
+        cell.textLabel?.text = options[indexPath.item]
         return cell
     }
-    */
+
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected cell number: \(indexPath.row)!")
+
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
+        }
+    }
+
+    @IBAction func done(_ sender: Any) {
+        // this calls a funcction in createDoItViewController,
+        // do something similar for the organization view controller
+        // delegate?.createDoItViewController(self, didSaveDoIt: doIt)
+        dismiss(animated: true)
+    }
 
     /*
     // Override to support conditional editing of the table view.
