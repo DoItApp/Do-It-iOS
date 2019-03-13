c89bcc2d94333bc1e70e8e016e27b746cb1d6bf6
mathmch <mathmch@gmail.com>
2019-03-12 17:24:36 -0700

    fully tested

--- Do-It/Extensions/UserDefaults.swift
+++ Do-It/Extensions/UserDefaults.swift
@@ -25,70 +25,84 @@ extension UserDefaults {
             var dateIndex = 0
             var priorityIndex = 0
             var filterSettings: [FilterSpecification] = []
-            let group = UserDefaults.standard.integer(forKey: Key.groupSetting)
-            let sort = UserDefaults.standard.integer(forKey: Key.sortSetting)
-            guard let courses = UserDefaults.standard.array(forKey: Key.courseOptions) as? [Course] else {return nil}
-            guard let dates = UserDefaults.standard.array(forKey: Key.dateOptions) as? [Date] else {return nil}
-            guard let priorities = UserDefaults.standard.array(forKey: Key.priorityOptions) as?
-                [DoItPriority] else {return nil}
-            guard let filters = UserDefaults.standard.array(forKey: Key.filterSetting)
-                                                            as? [FilterSetting] else {return nil}
+            let group = integer(forKey: Key.groupSetting)
+            let sort = integer(forKey: Key.sortSetting)
+            guard let courses = array(forKey: Key.courseOptions) as? [String] else {return nil}
+            guard let dates = array(forKey: Key.dateOptions) as? [Date] else {return nil}
+            guard let priorities = array(forKey: Key.priorityOptions) as?
+                [Int] else {return nil}
+            guard let filters = array(forKey: Key.filterSetting) as? [Int] else {return nil}
             for setting in filters {
                 switch setting {
-                case .course:
-                    filterSettings.append(CourseFilter(courses[courseIndex]))
+                case FilterSetting.course.rawValue:
+                    filterSettings.append(CourseFilter(Course(name: courses[courseIndex])))
                     courseIndex += 1
-                case .dueDate:
+                case FilterSetting.dueDate.rawValue:
                     filterSettings.append(DueDateFilter(firstDay: dates[dateIndex], lastDay: dates[dateIndex + 1]))
                     dateIndex += 2
-                case .priority:
-                    filterSettings.append(PriorityFilter(input: priorities[priorityIndex]))
-                    priorityIndex += 1
+                case FilterSetting.priority.rawValue:
+                    if let priority = DoItPriority(rawValue: priorities[priorityIndex]) {
+                        filterSettings.append(PriorityFilter(input: priority))
+                        priorityIndex += 1
+                    }
+                default:
+                    fatalError("Unknown sort setting")
+                }
+            }
+            var filtersAsEnum: [FilterSetting] = []
+            for elm in filters {
+                if let value = FilterSetting(rawValue: elm) {
+                    filtersAsEnum.append(value)
                 }
             }
             if let group = GroupingSetting(rawValue: group) {
                 return DoItOrganizationSettings(groupingSetting: group,
                                                 sortSetting: SortSetting(rawValue: sort),
                                                 filterSetting: filterSettings,
-                                                filters: filters)
+                                                filters: filtersAsEnum)
             }
             return nil
         }
 
         set {
-            if let settings = organizationSettings {
-                UserDefaults.standard.set(settings.groupingSetting.rawValue, forKey: Key.groupSetting)
-                if let sort = settings.sortSetting {
-                    UserDefaults.standard.set(sort, forKey: Key.sortSetting)
+            if let settings = newValue {
+                set(settings.groupingSetting.rawValue, forKey: Key.groupSetting)
+                if let sort = settings.sortSetting?.rawValue {
+                    set(sort, forKey: Key.sortSetting)
                 } else {
-                    UserDefaults.standard.set(-1, forKey: Key.sortSetting)
+                    set(-1, forKey: Key.sortSetting)
                 }
-                var courses: [Course] = []
+                var courses: [String] = []
                 var dueDates: [Date] = []
-                var priorities: [DoItPriority] = []
+                var priorities: [Int] = []
                 for setting in settings.filterSetting {
                     switch setting {
-                    case is CourseFilter:
-                        if let filter = setting as? CourseFilter {
-                            courses.append(filter.filterCourse)
-                        }
-                    case is DueDateFilter:
-                        if let filter = setting as? DueDateFilter {
-                            dueDates.append(filter.firstDate)
-                            dueDates.append(filter.lastDate)
-                        }
-                    case is PriorityFilter:
-                        if let filter = setting as? PriorityFilter {
-                            priorities.append(filter.filterPriority)
-                        }
+                    case let filter as CourseFilter:
+                        courses.append(filter.filterCourse.name)
+                    case let filter as DueDateFilter:
+                        dueDates.append(filter.firstDate)
+                        dueDates.append(filter.lastDate)
+                    case let filter as PriorityFilter:
+                        priorities.append(filter.filterPriority.rawValue)
                     default:
-                        return
+                        fatalError("unexpected filter type")
                     }
                 }
-                UserDefaults.standard.set(courses, forKey: Key.courseOptions)
-                UserDefaults.standard.set(dueDates, forKey: Key.dateOptions)
-                UserDefaults.standard.set(priorities, forKey: Key.priorityOptions)
-                UserDefaults.standard.set(settings.filters, forKey: Key.filterSetting)
+                set(courses, forKey: Key.courseOptions)
+                set(dueDates, forKey: Key.dateOptions)
+                set(priorities, forKey: Key.priorityOptions)
+                var filtersAsInt: [Int] = []
+                for elm in settings.filters {
+                    filtersAsInt.append(elm.rawValue)
+                }
+                set(filtersAsInt, forKey: Key.filterSetting)
+            } else {
+                removeObject(forKey: Key.groupSetting)
+                removeObject(forKey: Key.sortSetting)
+                removeObject(forKey: Key.filterSetting)
+                removeObject(forKey: Key.courseOptions)
+                removeObject(forKey: Key.dateOptions)
+                removeObject(forKey: Key.priorityOptions)
             }
         }
     }
---
+++ Do-ItTests/UserDefaultsTest.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefaultsTest.swift
+//  Do-ItTests
+//
+//  Created by user146350 on 3/12/19.
+//  Copyright © 2019 The Swifter Picker-Uppers. All rights reserved.
+//
+
+import Foundation
